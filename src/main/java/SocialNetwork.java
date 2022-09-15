5a7f657ce6da8450343bc7466b7e5a6c256169d7
brais <brais.garcia@codurance.com>
2022-09-15 15:09:14 +0200

    RED: Not reading

--- src/main/java/SocialNetwork.java
+++ src/main/java/SocialNetwork.java
@@ -1,2 +1,6 @@
 public class SocialNetwork {
+
+    public String execute(String command){
+        return "Posting";
+    }
 }
--- src/test/java/SocialNetworkShould.java
+++ src/test/java/SocialNetworkShould.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SocialNetworkShould {
+
+    SocialNetwork socialNetwork;
+
+    @Test
+    void post(){
+        String command = "Irene -> What a nice breakfast!";
+        socialNetwork = new SocialNetwork();
+        Assertions.assertEquals("Posting",socialNetwork.execute(command));
+    }
+
+    @Test
+    void read(){
+        String command = "Irene";
+        socialNetwork = new SocialNetwork();
+        Assertions.assertEquals("Reading",socialNetwork.execute(command));
+    }
+
+
 }
